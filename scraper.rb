7e4b939b765448aa3e10bae6a50537c0ea3c78c3
Sebastian <sebastian.dziadzio@gmail.com>
2014-08-05 19:19:30 +0200

    Using compact and map for a more DRY solution

--- scraper.rb
+++ scraper.rb
@@ -3,18 +3,7 @@ require 'nokogiri'
 require 'open-uri'
 
 def get_synonyms(word, n)
-  synonyms = []
   url = URI.encode("http://nlp.pwr.wroc.pl/wordnet/msr/#{word}")
-  page = Nokogiri::HTML(open(url))   
-  page.css('td.l').first(n).each do |elem|
-    synonyms.push(elem.text)
-  end
+  page = Nokogiri::HTML(open(url))
+  page.css('td.l').first(n).compact.map{|s| s.text}
 end
-
-
-
-f = File.open('words')
-f.readlines.each do |line|
-  puts get_synonyms(line, 5)
-end
-f.close
